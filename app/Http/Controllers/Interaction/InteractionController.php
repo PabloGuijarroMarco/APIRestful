60567c8128ad5135bbe112a3d7caebc281702427
Unknown <pabloguijarro96@gmail.com>
2019-09-26 14:03:00 +0200

    Index del controlador de ejemplo

--- app/Http/Controllers/Interaction/InteractionController.php
+++ app/Http/Controllers/Interaction/InteractionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Interaction;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Interaction;
 
 class InteractionController extends Controller
 {
@@ -14,7 +15,8 @@ class InteractionController extends Controller
      */
     public function index()
     {
-        //
+        $interacciones = Interaction::all();
+        return response()->json($interacciones);
     }
 
     /**
