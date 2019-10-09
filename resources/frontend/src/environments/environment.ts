2f8f4c98fd5392c7a42102b9f8b48c542bbbd181
Unknown <pabloguijarro96@gmail.com>
2019-10-09 10:32:37 +0200

    Animaciones y código de dialogflow añadidos

--- resources/frontend/src/environments/environment.ts
+++ resources/frontend/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  dialogflow:{
+    angularBot: '1b7bc39edb4d46e8aa1bfe222e87aa2c'
+  }
 };
 
 /*
