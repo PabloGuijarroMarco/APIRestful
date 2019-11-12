51c2558caf46060ee11ff4af5ea41def5578423b
Unknown <pabloguijarro96@gmail.com>
2019-11-12 13:04:34 +0100

    Ya no es sensible a mayúsculas ni a minúsculas

--- resources/frontend/src/app/components/chat/chat-dialog/chat-dialog.component.ts
+++ resources/frontend/src/app/components/chat/chat-dialog/chat-dialog.component.ts
@@ -522,7 +522,7 @@ export class ChatDialogComponent implements OnInit {
       buscar=this.daleya;
     }
     console.log(buscar);
-      this.sparqlQuery = 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?autor ?nombreautor WHERE { ?autor rdfs:label ?nombreautor . FILTER regex ((?nombreautor), "'+buscar+'") . }';
+      this.sparqlQuery = 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?autor ?nombreautor WHERE { ?autor rdfs:label ?nombreautor . FILTER regex ((?nombreautor), "'+buscar+'", "i") . }';
     
       this.fullUrl = '/bvmc-lod/repositories/data' + '?query=' + encodeURIComponent( this.sparqlQuery );
      console.log(this.fullUrl);
@@ -572,7 +572,7 @@ export class ChatDialogComponent implements OnInit {
     document.getElementById('u').innerText='Se está realizando la búsqueda...';
     let div = a.split("+");
     let buscar = div[2];
-      this.sparqlQuery = 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?autor ?nombreautor WHERE { ?autor rdfs:label ?nombreautor . FILTER regex ((?nombreautor), "'+buscar+'") . }';
+      this.sparqlQuery = 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?autor ?nombreautor WHERE { ?autor rdfs:label ?nombreautor . FILTER regex ((?nombreautor), "'+buscar+'", "i") . }';
     
       this.fullUrl = '/bvmc-lod/repositories/data' + '?query=' + encodeURIComponent( this.sparqlQuery );
      console.log(this.fullUrl);
